306188867af485621e8c3eab2962abac1e60aa34
Eduardo Rebola <eduardodanielli.eireli@poupex.com.br>
2024-08-20 21:46:14 -0300

    [genesis] create minimal rest-api module

---
+++ rest-api/src/main/java/com/github/erdanielli/petclinic/rest/RestApiAdapter.java
@@ -0,0 +1,13 @@
+package com.github.erdanielli.petclinic.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiAdapter.class, args);
+    }
+
+}
